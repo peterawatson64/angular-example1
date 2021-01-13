1b05c3074d28cc643d6194c1db299b4cdf3baefb
peterawatson64 <peterawatson64@gmail.com>
2021-01-13 16:37:59 +0000

    Notify me button function added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import {Input} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Input } from "@angular/core";
+import { Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  selector: "app-product-alerts",
+  templateUrl: "./product-alerts.component.html",
+  styleUrls: ["./product-alerts.component.css"]
 })
-export class ProductAlertsComponent implements OnInit {
-  @Input() product //The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
-  constructor() { }
+export class ProductAlertsComponent {
+  @Input() product; //The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
+  @Output() notify = new EventEmitter(); //To make the Notify Me button work, the child component needs to notify and pass the data to the parent component. The ProductAlertsComponent needs to emit an event when the user clicks Notify Me and the ProductListComponent needs to respond to the event.
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
