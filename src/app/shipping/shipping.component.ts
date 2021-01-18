04152ad4e07ad528cac599122eb75b111f146d1b
peterawatson64 <peterawatson64@gmail.com>
2021-01-18 10:40:31 +0000

    Shipping prices added

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.css']
 })
-export class ShippingComponent implements OnInit {
+export class ShippingComponent {
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
+
+  shippingCosts = this.cartService.getShippingPrices();
+
+
 
   ngOnInit() {
   }
